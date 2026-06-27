aafc6b7b380a201bdb503107dde0317fe64840dc
Bytangle <mredichaj@gmail.com>
2026-06-27 15:36:44 +0100

    release: v0.4.0

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const program = new Command()
 program
   .name('stx')
   .description('AI-powered documentation platform CLI')
-  .version('0.3.0')
+  .version('0.4.0')
 
 program.addCommand(initCommand)
 program.addCommand(devCommand)
